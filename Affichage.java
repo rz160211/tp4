47ed4ef960ee896145ef6d0544b7bfe5f4416e16
Rim <26852077+rz160211@users.noreply.github.com>
2018-05-27 05:16:50 +0200

    Add files via upload

--- Affichage.java
+++ Affichage.java
@@ -5,10 +5,82 @@
  */
 package Vue;
 
+import Controleur.ExceptionExistence;
+import Modele.*;
+import java.util.*;
+
 /**
  *
  * @author Rim
  */
 public class Affichage {
+    
+    public Affichage(){}
+    
+    /**
+     *
+     * @param hmapInstrument
+     * @return 
+     */
+    
+    /*Une méthode qui affiche, pour chaque instrument, 
+    sa clé, son nombre total de fonds et la somme totale des montants de ses fonds*/
+    
+    public List<Double> afficherInstrument(HashMap<String,Instrument> hmapInstrument)
+    {
+        Instrument instrument;
+        ArrayList<Fonds> collection;
+        List<Double> list = new ArrayList<>();
+        double valeur;
+        double somme = 0;
+        
+        for (HashMap.Entry<String,Instrument> entry : hmapInstrument.entrySet())
+        {
+            //affichage de la cle
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+            
+            //affichage du nombre total de fonds
+            instrument = entry.getValue();
+            collection = instrument.getStringList();
+            System.out.println("Cet instrument contient " + collection.size() + " fonds.");
+            
+            //affichage de la somme des fonds
+            for(Fonds amount : collection){
+                valeur = amount.getAmount();
+                somme = somme + valeur; 
+            }
+            
+            System.out.println("La somme est de " + somme + " .");
+            list.add(somme);
+        }
+        
+        return list;
+    }
+    
+    public List<Double> afficherPourcentage(String key, Portefeuille wallet, List<Double> list)
+    {
+        double pourcentage;
+        List<Double> pourcentageList = new ArrayList<>();
+        
+        try {
+            Fonds walletValue = wallet.rechercheFonds(key);
+            double value= walletValue.getAmount();
+            
+            //le pourcentage de chaque instrument pour ce fonds avec resultat de afficherInstrument            
+            for(double amount : list){
+                pourcentage = amount/value;
+                pourcentageList.add(pourcentage);
+            }
+            
+            
+        } catch (ExceptionExistence ex) {
+            //FondsInexistant est générée, message d'erreur
+            System.out.println("ERREUR 404: le fonds est inexistant.");
+
+        }
+        
+        return pourcentageList;
+    }
+    
     
 }
